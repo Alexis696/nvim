db4c4887cc2c9a14a84a6aa71df5f3a93875593e
Aleksis <alexinmercado68@gmail.com>
2026-07-03 02:53:56 +0200

    nvim config updated

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,6 +6,7 @@ return {
       require("mason").setup()
     end,
   },
+
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
@@ -13,16 +14,34 @@ return {
       auto_install = true,
     },
   },
+
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({capabilities = capabilities})
-      lspconfig.ts_ls.setup({capabilities = capabilities})
-      lspconfig.pyright.setup({capabilities = capabilities})
+      -- Configure language servers using the new recommended API
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = {
+              checkThirdParty = false,
+              library = { vim.fn.expand("$VIMRUNTIME/lua") },
+            },
+          },
+        },
+      })
 
+      vim.lsp.config("ts_ls", { capabilities = capabilities })
+      vim.lsp.config("pyright", { capabilities = capabilities })
+
+      -- Enable the language servers
+      vim.lsp.enable({ "lua_ls", "ts_ls", "pyright" })
+
+      -- Your keymaps (kept exactly as before)
       vim.keymap.set("n", "gh", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,23 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-      auto_install = true,
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+
+  dependencies = {
+    "mks-h/treesitter-autoinstall.nvim",   -- ← this does the auto-install
+  },
+
+  config = function()
+    require("nvim-treesitter").setup({
+      -- Optional: change install directory if you want
+      -- install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    -- Auto-install parsers when you open a new filetype
+    require("treesitter-autoinstall").setup({
+      ignore = {},        -- languages you never want to auto-install
+      highlight = true,   -- auto-enable highlighting for installed languages
+      regex = {},         -- extra languages to use regex highlighting for
+    })
+  end,
 }
